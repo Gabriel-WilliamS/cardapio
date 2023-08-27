c37bd666a0986ae6ffe1087474dd1a17d1e93833
Gabriel-WilliamS <gabriel.serrano@eumedicoresidente.com.br>
2023-08-26 22:13:59 -0300

    📝 chore(migration): add migration to make 'name' column in 'users' table required
    📝 chore(migration): add migration to create all tables: 'menus', 'products', 'menus_on_products', 'categories'
    
    📝 chore(schema.prisma): update 'User' model to have a one-to-many relationship with 'Menu' model
    
    📝 chore(schema.prisma): add 'Menu' model with relationships to 'User' and 'MenusOnProducts' models
    
    📝 chore(schema.prisma): add 'Product' model with relationships to 'Category' and 'MenusOnProducts' models
    
    📝 chore(schema.prisma): add 'MenusOnProducts' model with relationships to 'Menu' and 'Product' models
    
    📝 chore(schema.prisma): add 'Category' model with relationship to 'Product' model

---
+++ prisma/migrations/20230827011129_create_all_tables/migration.sql
@@ -0,0 +1,58 @@
+-- CreateTable
+CREATE TABLE "menus" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "menus_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "products" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "description" TEXT NOT NULL,
+    "categoryId" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "status" BOOLEAN NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "menus_on_products" (
+    "id" TEXT NOT NULL,
+    "menuId" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "menus_on_products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "menus" ADD CONSTRAINT "menus_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "products" ADD CONSTRAINT "products_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "menus_on_products" ADD CONSTRAINT "menus_on_products_menuId_fkey" FOREIGN KEY ("menuId") REFERENCES "menus"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "menus_on_products" ADD CONSTRAINT "menus_on_products_productId_fkey" FOREIGN KEY ("productId") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
